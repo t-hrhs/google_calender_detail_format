7091a6c8e4952c73942979de9b05ae7df808e935
t-hrhs <hirohashi.t.0204@gmail.com>
2018-11-17 23:33:44 +0900

    全部一旦追加

---
+++ app/scripts.babel/contentscript.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var href = undefined;
+
+var observer = new MutationObserver(function(mutations) {
+  if((href !== location.href || href === undefined ) && document.URL.match(/eventedit/)) {
+    console.log('Before:', href);
+    console.log('After:', location.href);
+    href = location.href;
+    $(function() {
+      console.log(document.URL);
+      var intervalCount = 0;
+      var id = setInterval(function(){
+      ++intervalCount;
+      console.log('interval now');
+      var $scheduleDescDOM = $('div[id^="hInySc"]');
+      var $scheduleDummyDOM = $('.iSSROb');
+      if ($scheduleDescDOM[0] && $scheduleDummyDOM[0] || intervalCount > 100)
+      {
+        clearInterval(id);
+        var text = $scheduleDescDOM.text();
+        if (text)
+        {
+          console.log($scheduleDescDOM.text());
+          console.log('更新する必要なし');
+        }
+        else
+        {
+          console.log('変更する必要ある');
+          // NOTE : 暫定対応
+          $scheduleDummyDOM.text('');
+          $scheduleDescDOM.text('changed text');
+        }
+      }
+    },100);
+  });
+  }
+});
+observer.observe(document, { childList: true, subtree: true });
